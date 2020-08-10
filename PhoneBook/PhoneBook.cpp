7f6579207cae951f47255467cd7541205a92d2b9
KULINININ <aleks680026032001@gmail.com>
2020-08-10 20:13:02 +0500

    Create a "structure"

--- PhoneBook/PhoneBook.cpp
+++ PhoneBook/PhoneBook.cpp
@@ -1,6 +1,54 @@
 ﻿#include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
+
+    while (true)
+    {
+        std::string input;
+        std::cin >> input;
+
+        if (input == "1")
+        {
+            //List();
+        }
+        else if (input == "2")
+        {
+            //AddContact();
+        }
+        else if (input == "3")
+        {
+            //DeleteContact();
+        }
+        else if (input == "4")
+        {
+            //EditContact();
+        }
+        else if (input == "5")
+        {
+            //ListOfGroup();
+        }
+        else if (input == "6")
+        {
+            //AddGroup();
+        }
+        else if (input == "7")
+        {
+            //DeleteGroup();
+        }
+        else if (input == "8")
+        {
+            //EditGroup();
+        }
+        else if (input == "9")
+        {
+            //Save
+        }
+        else if (input == "10")
+        {
+            //Save and exit
+        }
+    }
 }
