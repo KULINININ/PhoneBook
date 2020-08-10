ad1f02549e1ad633b51e3934dccb1f240939a31e
KULINININ <aleks680026032001@gmail.com>
2020-08-10 20:15:24 +0500

    Create class Person

--- PhoneBook/PhoneBook.cpp
+++ PhoneBook/PhoneBook.cpp
@@ -1,6 +1,36 @@
 ﻿#include <iostream>
 #include <string>
 
+class Person
+{
+private:
+    int id = 0;
+    std::string number;
+    std::string name;
+    std::string lastname;
+
+public:
+    void setName(std::string _name)
+    {
+        name = _name;
+    }
+
+    void setLastName(std::string _lastname)
+    {
+        lastname = _lastname;
+    }
+
+    void setNumber(std::string _number)
+    {
+        number = _number;
+    }
+
+    void setId(int _id)
+    {
+        id = _id;
+    }
+};
+
 int main()
 {
     setlocale(LC_ALL, "rus");
